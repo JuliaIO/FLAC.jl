f96bdfc4f8d438bfac9cc41da2533a5702c675a3
Elliot Saba <staticfloat@gmail.com>
2016-12-21 07:04:37 -0800

    Add `FLACDecoder` type that simplified seeking and streaming (#9)
    * Add `FLACDecoder` type that simplified seeking and streaming
    
    * Remove excess `begin` at the end of `if` statement
    
    * Add requirement on Homebrew on OSX
    
    * Add `disable_sigint()` to FLAC library calls that call back into Julia
    
    * Add appveory config script
    
    * Get windows CI working
    
    * Add `gc()` before trying to `rm()` a file to ensure
    
    This is necessary on windows, because windows doesn't like delete'ing
    files that are still opened by a process.  Running `gc()` ensures that
    the file handle objects get garbage collected before attempting to
    `rm()` them.
    
    * Julia 0.4 is not supported by FLAC, so don't test on it
    
    * Since we're on 0.5 anyway, let's use the neato `@testset` machinery
    
    * Use better templating to work on 32 and 64 bit

--- deps/build.jl
+++ deps/build.jl
@@ -3,21 +3,26 @@ using Compat
 
 @BinDeps.setup
 
-libflac = library_dependency("libflac",aliases=["libFLAC"])
-libogg = library_dependency("libogg")
-libvorbis = library_dependency("libvorbis")
+libflac = library_dependency("libflac",aliases=["libFLAC","libFLAC-8"])
+libogg = library_dependency("libogg",aliases=["libogg-0"])
+libvorbis = library_dependency("libvorbis",aliases=["libvorbis-0"])
 
 provides(AptGet,"libflac-dev",libflac)
 provides(AptGet,"libogg-dev",libogg)
 provides(AptGet,"libvorbis-dev",libvorbis)
 
-@static if is_apple() begin
-    if Pkg.installed("Homebrew") === nothing
-        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")  end
+@static if is_apple()
     using Homebrew
     provides( Homebrew.HB, "flac", libflac, os = :Darwin )
     provides( Homebrew.HB, "libogg", libogg, os = :Darwin )
     provides( Homebrew.HB, "libvorbis", libvorbis, os = :Darwin )
+end
+
+@static if is_windows()
+    using WinRPM
+    provides(WinRPM.RPM, "flac", libflac, os = :Windows )
+    provides(WinRPM.RPM, "libogg0", libogg, os = :Windows )
+    provides(WinRPM.RPM, "libvorbis0", libvorbis, os = :Windows )
 end
 
 @BinDeps.install Dict(:libflac => :libflac,:libogg => :libogg)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,51 +1,74 @@
 using FileIO
 using WAV
 using Base.Test
-
-testdir = dirname(@__FILE__)
-
-# Start by loading our ground truth data from the .wav
-# This data generated as sin(linspace(0,4410*2*pi,44100)[1:441])
-check_data, check_fs = wavread(joinpath(testdir, "4410hz.wav"))
-
-# Load flac'ed version of 4410hz.wav (generated with `ffmpeg -i 4410hz.wav -acodec flac 4410hz.flac`)
-data, fs = load(joinpath(testdir, "4410hz.flac"))
-@test fs == check_fs
-@test size(data) == (441,1)
-@test maximum(abs(check_data - data)) < 1e-6
+using FLAC
 
 
-# Test against 16-bit FLAC file (generated with `ffmpeg -i 4410hz.wav -acodec flac -sample_fmt s16 4410hz_s16.flac`)
-s16_data, s16_fs = load(joinpath(testdir, "4410hz_s16.flac"))
-@test s16_fs == check_fs
-@test size(s16_data) == (441,1)
-@test maximum(abs(check_data - s16_data)) < 1e-4   # reduced precision due to s16 format
+@testset "FLAC" begin
+    testdir = dirname(@__FILE__)
+
+    # Start by loading our ground truth data from the .wav
+    # This data generated as sin(linspace(0,4410*2*pi,44100)[1:441])
+    check_data, check_fs = wavread(joinpath(testdir, "4410hz.wav"))
+
+    # Load flac'ed version of 4410hz.wav (generated with `ffmpeg -i 4410hz.wav -acodec flac 4410hz.flac`)
+    data, fs = load(joinpath(testdir, "4410hz.flac"))
+    @test fs == check_fs
+    @test size(data) == size(check_data)
+    @test maximum(abs(check_data - data)) < 1e-6
 
 
-# Test multichannel FLAC file (left and right channels opposing polarity versions of 4410hz signal)
-stereo_data = load(joinpath(testdir, "stereo.flac"))[1]
-stereo_check_data = wavread(joinpath(testdir, "stereo.wav"))[1]
-@test size(stereo_data) == size(stereo_check_data)
-@test maximum(abs(stereo_data - stereo_check_data)) < 1e-6
+    # Test against 16-bit FLAC file (generated with `ffmpeg -i 4410hz.wav -acodec flac -sample_fmt s16 4410hz_s16.flac`)
+    s16_data, s16_fs = load(joinpath(testdir, "4410hz_s16.flac"))
+    @test s16_fs == check_fs
+    @test size(s16_data) == size(check_data)
+    @test maximum(abs(check_data - s16_data)) < 1e-4   # reduced precision due to s16 format
 
 
-# Now that we have confidence our decoder works, let's roundtrip a signal multiple times
-function roundtrip(signal, samplerate; params...)
-    path = joinpath(testdir,"roundtrip.flac")
-    if isfile(path)
+    # Test multichannel FLAC file (left and right channels opposing polarity versions of 4410hz signal)
+    stereo_data = load(joinpath(testdir, "stereo.flac"))[1]
+    stereo_check_data = wavread(joinpath(testdir, "stereo.wav"))[1]
+    @test size(stereo_data) == size(stereo_check_data)
+    @test maximum(abs(stereo_data - stereo_check_data)) < 1e-6
+
+    # Test FLACDecoder works with chunked reads
+    f = FLACDecoder(joinpath(testdir, "4410hz.flac"))
+    chunked = Array{Float32,2}(size(f)...)
+    chunked[1:100, :] = read(f, 100)
+    chunked[101:end, :] = read(f, length(f) - 100)
+    @test f.metadata.samplerate == check_fs
+    @test size(f) == size(check_data)
+    @test maximum(abs(check_data - chunked)) < 1e-6
+
+    # Test FLACDecoder seek'ing works
+    f = FLACDecoder(joinpath(testdir, "4410hz.flac"))
+    seek(f, 100)
+    @test maximum(abs(read(f, 100) - check_data[101:200])) < 1e-6
+    @test maximum(abs(read(f, 100) - check_data[201:300])) < 1e-6
+    seek(f, 0)
+    @test maximum(abs(read(f, length(f)) - check_data)) < 1e-6
+
+
+    # Now that we have confidence our decoder works, let's roundtrip a signal multiple times
+    function roundtrip(signal, samplerate; params...)
+        path = joinpath(testdir,"roundtrip.flac")
+        if isfile(path)
+            gc()
+            rm(path)
+        end
+        save(path, signal, samplerate; params...)
+        recon_signal, recon_fs = load(path)
+
+        @test recon_fs == samplerate
+        @test size(recon_signal) == size(signal)
+        #show(recon_signal - signal)
+        @test maximum(abs(recon_signal - signal)) < 1e-4
+        gc()
         rm(path)
     end
-    save(path, signal, samplerate; params...)
-    recon_signal, recon_fs = load(path)
 
-    @test recon_fs == samplerate
-    @test size(recon_signal) == size(signal)
-    #show(recon_signal - signal)
-    @test maximum(abs(recon_signal - signal)) < 1e-4
-    rm(path)
+    test_signal = sin(linspace(0,4410*2*pi,44100)[1:44100])''*.999
+    roundtrip(test_signal, 44100)
+    roundtrip(test_signal, 44100, bits_per_sample=16)
+    roundtrip(test_signal, 48000, compression_level=8)
 end
-
-test_signal = sin(linspace(0,4410*2*pi,44100)[1:44100])''*.999
-roundtrip(test_signal, 44100)
-roundtrip(test_signal, 44100, bits_per_sample=16)
-roundtrip(test_signal, 48000, compression_level=8)
