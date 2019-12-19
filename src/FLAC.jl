3a743a15c4f5741cbfe4c10aeca67651c1d80dc2
Rowan Katekar <rowan.katekar@dolby.com>
2019-12-20 10:45:59 +1100

    add FLAC_jll as a dependency

--- src/FLAC.jl
+++ src/FLAC.jl
@@ -3,21 +3,7 @@ __precompile__()
 module FLAC
 
 using FileIO
-
-# This `OggWrapper` is literally only just so that `libogg` is loaded into the
-# Julia namespace, which is necessary for `libFLAC` to load properly.
-# It looks like the original issue https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/194
-# has been solved, so we just need to make another BinaryBuilder release of libFLAC
-# and then this can be deleted.
-module OggWrapper
-    import Ogg
-    const depfile = joinpath(dirname(pathof(Ogg)), "..", "deps", "deps.jl")
-    if isfile(depfile)
-        include(depfile)
-    else
-        error("Ogg not properly installed. Please run Pkg.build(\"Ogg\")")
-    end
-end
+using FLAC_jll
 
 export StreamMetaData,
        InfoMetaData,
@@ -37,20 +23,12 @@ export StreamMetaData,
 
 import Base: read, seek, size, length
 
-const depfile = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if isfile(depfile)
-    include(depfile)
-else
-    error("FLAC not properly installed. Please run Pkg.build(\"FLAC\")")
-end
-
 include("metadata.jl")
 include("format.jl")
 include("decoder.jl")
 include("encoder.jl")
 
 function __init__()
-    OggWrapper.check_deps()
     init_decoder_cfunctions()
     init_encoder_cfunctions()
 end
